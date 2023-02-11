753e4105d890fa7bad2529a0e6f990fe1ba83417
Carlos Caballero <carlosjocaballero@gmail.com>
2023-02-11 11:56:59 -0500

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 use std::io::Write;
 use std::env;
 
+// NOTE: This should be part of a struct. It was made this way for testing purposes
+
 fn main() {
     let args : Vec<_> = env::args().collect();
 
@@ -19,9 +21,11 @@ fn main() {
 }
 
 fn run_file(path: &String){
+    // Reads the file as a string and send it to the run() function
     run(fs::read_to_string(path).expect("ERROR: Could not read file. Check directory is right or that the file is in the root folder"));
 }
 
+// Opens up the REPL
 fn run_prompt(){
     loop{
         print!(">> ");
@@ -33,6 +37,7 @@ fn run_prompt(){
     }
 }
 
+// NOTE: Unfinished. Tokenizing needed in run()
 fn run(source: String){
     println!("{}", source);
 }
