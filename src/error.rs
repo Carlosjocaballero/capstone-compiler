2d8046c8a156d8524f9a3ac26095a7ea04f8ed6b
Carlosjocaballero <carlosjocaballero@gmail.com>
2023-02-18 19:02:04 -0500

    Finished Scanner
    Implemented basic error checking; it tells the line the error is in. More optimization is required

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+pub struct ScannerError{
+    pub is_error: bool
+}
+
+impl ScannerError{
+    pub fn _error(&mut self, line :i32, message: String){
+        self.report(line, "".to_string(), message)
+    }
+
+    pub fn report(&mut self, line: i32, _where: String, message: String){
+        println!("[line {}] Error {}: {}", line, _where, message);
+        self.is_error = true;
+    }
+}
+
