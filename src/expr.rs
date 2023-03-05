6a48ceb4246e172015b2c9efd3175a3baa8d8de4
Carlos J Caballero <56653674+Carlosjocaballero@users.noreply.github.com>
2023-03-05 16:12:20 -0500

    Update expr.rs

--- src/expr.rs
+++ src/expr.rs
@@ -119,15 +119,3 @@ impl CloneExpr {
         visitor.visit_clone_expr(self)
     }
 }
-
-trait InstanceOf
-where
-    Self: Any,
-{
-    fn instance_of<U: ?Sized + Any>(&self) -> bool {
-        TypeId::of::<Self>() == TypeId::of::<U>()
-    }
-}
-
-// implement this trait for every type that implements `Any` (which is most types)
-impl<T: ?Sized + Any> InstanceOf for T {}
