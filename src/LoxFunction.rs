62151ed02f563cab7c36d104c357183f56d17733
Aziz <abdoulniang98@myhunter.cuny.edu>
2023-03-14 00:03:38 -0400

    Function Update

--- src/LoxFunction.rs
+++ src/LoxFunction.rs
@@ -1,37 +1,29 @@
+use crate::stmt::FunctionStmt;
 use crate::token::*;
-use crate::expr::*;
 use crate::interpreter::*;
-use crate::LoxCallable::LoxCallable;
 use crate::Environment;
-use crate::Stmt;
-
-//Draft
 
 pub struct LoxFunction{
-    pub declaration:Stmt::Function,
+    pub declaration:FunctionStmt,
 }
-
 impl LoxFunction{
-    pub fn LoxFunction(&mut self, declaration:Stmt::Function){
+    pub fn LoxFunction(&mut self, declaration:FunctionStmt){
         self.declaration = declaration;
     }
-}
-
-dyn impl LoxFunction for LoxCallable{
-    fn call(&mut self, interpreter: &Interpreter, arguments: Vec<Literal>) -> Literal{
+    pub fn call(&mut self, interpreter: &Interpreter, arguments: Vec<Literal>) -> Literal{
         let environment= Box::new(Environment::new_enclosed(&interpreter.globals));
         let i = 0;
-        while i<self.declaration.params.size(){
-            environment.define(self.declaration.params.get(i).lexeme, arguments(i));
+        while i<self.declaration.parameters.len(){
+            environment.define(self.declaration.parameters.get(i).lexeme, arguments(i));
             i = i + 1;
         }
-        interpreter.execute_block(self.declaration.body, environment);
-        return None;
+        interpreter.execute_block(&self.declaration.body, environment);
+        return Literal::None;
     }
-    fn arity(&mut self) -> Literal{
-        return self.declaration.params.size();
+    pub fn arity(&mut self) -> usize{
+        return self.declaration.parameters.len();
     }
-    fn toString(&mut self) -> String{
-        return "<fn" + self.declaration.name.lexeme + ">";
+    pub fn toString(&mut self) -> String{
+        return "<fn".to_string() + &self.declaration.name.lexeme + ">";
     }
-} 
+}
